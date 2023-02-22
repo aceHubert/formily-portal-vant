c5c09c6a39e1c569f93c13231095519bf35ac2ff
hubert <hubert@lejian.com>
2023-02-22 18:48:58 +0800

    feat: html-content 添加富文本编辑

--- packages/prototypes/src/schemas/HtmlContent.ts
+++ packages/prototypes/src/schemas/HtmlContent.ts
@@ -3,11 +3,11 @@ import type { ISchema } from '@formily/vue'
 export const HtmlContent: ISchema = {
   type: 'object',
   properties: {
-    content: {
-      type: 'string',
-      'x-decorator': 'FormItem',
-      'x-component': 'Input.TextArea',
-      'x-component-props': {},
-    },
+    // content: {
+    //   type: 'string',
+    //   'x-decorator': 'FormItem',
+    //   'x-component': 'Input.TextArea',
+    //   'x-component-props': {},
+    // },
   },
 }
