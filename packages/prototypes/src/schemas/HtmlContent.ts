169a3417776d61617152ee568b9ed54a5fd394a7
hubert <hubert@lejian.com>
2023-10-16 10:23:33 +0800

    fix: html-content edit problem

--- packages/prototypes/src/schemas/HtmlContent.ts
+++ packages/prototypes/src/schemas/HtmlContent.ts
@@ -3,11 +3,13 @@ import type { ISchema } from '@formily/vue'
 export const HtmlContent: ISchema = {
   type: 'object',
   properties: {
-    // content: {
-    //   type: 'string',
-    //   'x-decorator': 'FormItem',
-    //   'x-component': 'Input.TextArea',
-    //   'x-component-props': {},
-    // },
+    content: {
+      type: 'string',
+      'x-decorator': 'FormItem',
+      'x-component': 'Input.TextArea',
+      'x-component-props': {
+        rows: 4,
+      },
+    },
   },
 }
